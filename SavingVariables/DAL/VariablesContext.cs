1f864f8420b79e7fc9cdb60f4bf09367fab1ca42
ZachParris <zacharyjparris@gmail.com>
2016-09-16 19:05:18 -0500

    Added ERD, Model, and Context

---
+++ SavingVariables/DAL/VariablesContext.cs
@@ -0,0 +1,15 @@
+﻿using SavingVariables.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingVariables.DAL
+{
+    public class VariablesContext : DbContext
+    {
+        public virtual DbSet<Variables> Variable { get; set; }
+    }
+}
---
+++ SavingVariables/DAL/VariablesRepository.cs
@@ -0,0 +1,29 @@
+﻿using SavingVariables.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingVariables.DAL
+{
+    class VariablesRepository
+    {
+        public VariablesContext Context { get; set; }
+
+        public VariablesRepository()
+        {
+            Context = new VariablesContext();
+        }
+
+        public VariablesRepository(VariablesContext _context)
+        {
+            Context = _context;
+        }
+
+        public List<Variables> GetVariables()
+        {
+            return Context.Variable.ToList();
+        }
+    }
+}
