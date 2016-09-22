5bf3cd94019b35156c8148e63fe53f536d0383de
ZachParris <zacharyjparris@gmail.com>
2016-09-22 17:12:57 -0500

    Adding basic tests for each new class

---
+++ SavingVariables.Tests/ConstantsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SavingVariables.Tests
+{
+    [TestClass]
+    public class ConstantsTests
+    {
+        [TestMethod]
+        public void ConstCanBeInstantiated()
+        {
+            Constants tester = new Constants();
+            Assert.IsNotNull(tester);
+        }
+        [TestMethod]
+        public void ConstantsCanBeAddedToDictionary()
+        {
+            Constants tester = new Constants();
+            char x = default(char);
+            tester.AddConstantsToRepository(x, 1);
+            Assert.AreEqual(tester.GetConstant(x), 1);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExceptionThrownForExistingVar()
+        {
+            Constants tester = new Constants();
+            char x = default(char);
+            tester.AddConstantsToRepository(x, 1);
+            tester.AddConstantsToRepository(x, 2);
+        }
+    }
+}
